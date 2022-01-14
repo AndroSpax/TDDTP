3b9c05b2cd5e18a58d456b60e194f35bd0f4bc5e
Marielle <marielle.bouchoux@gmail.com>
2022-01-14 17:05:11 +0100

    findbyid

--- com.store/src/main/java/com/store/controller/ArticleController.java
+++ com.store/src/main/java/com/store/controller/ArticleController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +30,11 @@ public class ArticleController {
 	public List<Article> getAllArticle(){
 		return articlePersistence.findAll();
 	}
+	
+	@GetMapping("/{id}")
+	public void findArticleById(@PathVariable("id") Long id) {
+		Article article=articlePersistence.findById(id).get();
+		System.out.println(article);
+	}
+	
 }
