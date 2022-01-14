dac39c90271658ba34754aad56673b0ddfaf6356
AndroSpax <63066048+AndroSpax@users.noreply.github.com>
2022-01-14 15:24:55 +0100

    Modele Entity Article Good

---
+++ com.store/src/main/java/com/store/article/Article.java
@@ -0,0 +1,52 @@
+package com.store.article;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Article {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String title;
+	private String link;
+	private Integer votes; 
+	
+	public String getTitle() {
+		return title;
+	}
+	public Integer getVotes() {
+		return votes;
+	}
+	public void setVotes(Integer votes) {
+		this.votes = votes;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	
+	public Article() {
+		super();
+	}
+	public Article(Long id, String title, String link, Integer votes) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.link = link;
+		this.votes = votes;
+	}
+	
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", title=" + title + ", link=" + link + "]";
+	}
+}
